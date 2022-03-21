f8e2b4fb0d85f1cc87b04a25bf8192a18edfa7a1
JochenFM <jochen_mayer@gmx.de>
2022-03-21 19:45:35 +0000

    function add block complete with first block created via for loop

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const grid = document.querySelector('.grid')
-const blockWith = 100
+const blockWidth = 100
 const blockHeight = 20 
+
 //create Block
 
-class Block{
-    constructor(xAxis, yAxis)
+class Block{ //to create the blocks in the grid by passing through 2 values
+    constructor(xAxis, yAxis) {
+        this.bottomLeft = [xAxis, yAxis]//block as anchor point
+        this.bottomRight = [xAxis + blockWidth, yAxis]
+        this.topLeft = [xAxis, yAxis + blockHeight]
+        this.topRight = [xAxis + blockWidth, yAxis + blockHeight]
+    }
 }
 
-//draw my block'
-function addBlock() {
+
+//all my blocks
+const blocks = [
+    new Block(10, 270)
+]
+
+//draw all my blocks
+function addBlocks() {       
+for (let i = 0; i < blocks.length; i++){ //as long as i is smaller than the blocks array length, we increment i by 1
 const block = document.createElement('div')//create a div, store it as block
 block.classList.add('block')//give the block the class list as styled in CSS
-block.style.left = '100px'
-block.style.bottom = '50px'
-grid.appendChild(block) //put in the newly created block with the style of block       
+block.style.left = blocks[i].bottomLeft[0] + 'px'//style the first item in the blocks array, with the bottom left as anchor point i.e., 10px as result
+block.style.bottom = blocks[i].bottomLeft[1] +'px'//style the first item in the blocks array, with the bottom left as anchor point i.e., 270px as result
+grid.appendChild(block) //put in the newly created block with the style of block
+    }
 }
-addBlock()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+addBlocks()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
