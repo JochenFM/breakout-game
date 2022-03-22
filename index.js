eeadb17300c8b17afed1ae4b10466e869134cef6
JochenFM <jochen_mayer@gmx.de>
2022-03-22 19:14:52 +0000

    changeDirection Function for all directions except ?? and checkForCollision function for ball to stay within board

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const grid = document.querySelector('.grid')
+const scoreDisplay = document.querySelector('#score')
 const blockWidth = 100;
 const blockHeight = 20;
+const ballDiameter = 20;
 const boardWidth = 560;
+boardHeight = 300;
+let timerId
+let xDirection = 2
+let yDirection = 2
 
 const userStart = [230, 10]//default start position user, 230px, 10px
 let currentPosition = userStart
@@ -106,10 +112,53 @@ grid.appendChild(ball)//grab ball inside the HTML grid class
 // move ball
 
 function moveBall() {
-    ballCurrentPosition[0] +=2
-    ballCurrentPosition[1] +=2
+    ballCurrentPosition[0] += xDirection
+    ballCurrentPosition[1] += yDirection
     drawBall()
+    checkForCollisions()
 
 }
 
-setInterval(moveBall, 30) //invoke the function every 30ms
+timerId = setInterval(moveBall, 30) //invoke the function every 30ms
+
+//function check for collisions
+
+function checkForCollisions() {
+    //check for wall collisions
+    if (
+        ballCurrentPosition[0] >= (boardWidth - ballDiameter) ||
+        ballCurrentPosition[1] >= (boardHeight - ballDiameter) ||
+        ballCurrentPosition[0] <= 0
+        ){
+        changeDirection()
+    }
+
+
+//check for game over
+
+    if (ballCurrentPosition[1] <= 0) {
+        clearInterval(timerId)
+        scoreDisplay.innerHTML = 'You lose'
+        document.removeEventListener('keydown', moveUser)
+    }
+}
+
+
+function changeDirection() {
+    if (xDirection === 2 && yDirection === 2) {
+        yDirection = -2
+        return
+    }
+    if (xDirection === 2 && yDirection === -2) {
+        xDirection = -2
+        return
+    }
+    if (xDirection === -2 && yDirection === -2) {
+        yDirection = 2
+        return
+    }
+    if (xDirection === -2 && yDirection === 2) {
+        xDirection = 2
+        return
+    }
+}
